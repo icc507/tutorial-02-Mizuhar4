ac90cadecfbefb06dd18a980bc019d7540286397
Benjamín Fernández <142475169+Mizuhar4@users.noreply.github.com>
2024-03-22 10:34:12 -0300

    act 3

--- problema3.py
+++ problema3.py
@@ -9,5 +9,46 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+def insert_trinary(root, num):
+    if not root:
+        return [num, [], [], []]
+    if num < root[0]:
+        root[1] = insert_trinary(root[1], num)
+    elif num == root[0]:
+        if not root[2]:
+            root[2] = []
+        root[2].append(num)
+    else:
+        root[3] = insert_trinary(root[3], num)
+    return root
+
+def construct_trinary_tree(numbers):
+    numbers = numbers.split()
+    valid_numbers = []
+    for num in numbers:
+        num = ''.join(filter(str.isdigit, num))
+        if num:
+            valid_numbers.append(int(num))
+    root = None
+    for num in valid_numbers:
+        root = insert_trinary(root, num)
+    return root
+
+def print_trinary_tree(root):
+    if not root:
+        return
+    print("[", root[0], end="")
+    if root[1] or root[2] or root[3]:
+        print(", ", end="")
+    print_trinary_tree(root[1])
+    if root[2]:
+        print(", ", root[2], end="")
+    if root[3]:
+        print(", ", end="")
+        print_trinary_tree(root[3])
+    print("]", end="")
+
+if __name__ == "__main__":
+    numbers = input()
+    trinary_tree = construct_trinary_tree(numbers)
+    print_trinary_tree(trinary_tree)
